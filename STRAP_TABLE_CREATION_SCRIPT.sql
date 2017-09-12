c60b1d6cf1e315be70257d61d5c152bac63632db
saifgubbi <saifgubbi@gmail.com>
2017-09-12 14:27:30 +0530

    First comment

---
+++ STRAP_TABLE_CREATION_SCRIPT.sql
@@ -0,0 +1,256 @@
+/********************************************************************************************************************************************************************
+  **  Name       : STRAP_TABLE_CREATION_SCRIPT
+  **  DESCRIPTION: This script is used to create all the tables in Bosch Strap
+  **  MODIFICATION  HISTORY
+  **  Change Number   Version  Date           Modified By      Remarks
+  **  -               1.0      17-Aug-2017    Saif             Initial draft version
+  ************************************************************************************************************************************************************/
+
+--Drop All Tables 
+
+DROP TABLE EVENTS_T;
+DROP TABLE BINS_T;
+DROP TABLE PALLETS_T;
+DROP TABLE USERS_T;
+DROP TABLE PARTS_T;
+DROP TABLE ASN_T;
+DROP TABLE LOCATIONS_T;
+DROP TABLE SCHED_T;
+-- DROP TABLE INV_PALLET_T;
+DROP TABLE UPLOAD_LOG_T;
+DROP TABLE USER_PARTS_T;
+DROP TABLE PARTS_GRP_T;
+DROP TABLE INV_HDR_T;
+DROP TABLE INV_LINE_T;
+DROP TABLE SERIAL_T;
+--Creation of EVENTS_T Table
+
+  CREATE TABLE STRAP.EVENTS_T
+   (  EVENT_ID                  VARCHAR2(20), 
+	  EVENT_TYPE                VARCHAR2(20), 
+	  EVENT_NAME                VARCHAR2(20), 
+	  EVENT_DATE                DATE, 
+	  FROM_LOC                  VARCHAR2(5), 
+      TO_LOC                    VARCHAR2(5),
+	  LABEL                     VARCHAR2(20), 
+	  PART_NO                   VARCHAR2(16), 
+	  QTY                       NUMBER, 
+	  INVOICE_NUM               VARCHAR2(20), 
+	  USER_ID                   VARCHAR2(20) NOT NULL, 
+	  COMMENTS                  VARCHAR2(2000), 
+	  SEQ                       NUMBER, 
+	  EVENT_TS                  NUMBER, 
+	  REF_ID                    VARCHAR2(20),
+	  REF_LABEL                 VARCHAR2(20),
+      PART_GRP                  VARCHAR2(4),
+      LR_NO                     VARCHAR2(20),
+      DEVICE_NO                 VARCHAR2(20),
+	  SERIAL_NUM	            NUMBER,
+    CONSTRAINT PK_EVENTS primary key(EVENT_ID, EVENT_TYPE,EVENT_NAME,SEQ,EVENT_TS)
+   ) ;
+
+-- CREATE OR REPLACE SYNONYM APPS.EVENTS_T FOR STRAP.EVENTS_T;
+
+-- Create Table BINS_T
+
+--alter table bins_t modify bin_id NUMBER  VARCHAr2(16);
+CREATE TABLE STRAP.BINS_T 
+   (  BIN_ID                    VARCHAR2(16) NOT NULL , 
+	  STATUS                    VARCHAR2(20) NOT NULL , 
+	  STATUS_DT                 DATE, 
+	  FROM_LOC                  VARCHAR2(5) NOT NULL , 
+	  PALLET_ID                 NUMBER, 
+      LABEL                     VARCHAR2(20), 
+	  INVOICE_NUM               VARCHAR2(20),
+	  STATE                     NUMBER DEFAULT 1 NOT NULL, -- To Indicate Active/Inactive
+	  PART_NO                   VARCHAR2(20), 
+	  QTY                       NUMBER DEFAULT 0 NOT NULL, 
+	  OWNER                     VARCHAR2(20) NOT NULL , 
+	  SEQ                       NUMBER DEFAULT 0 NOT NULL ,
+      PART_GRP                  VARCHAR2(4),
+	 CONSTRAINT PK_BINS PRIMARY KEY (BIN_ID)
+    );
+
+-- CREATE OR REPLACE SYNONYM APPS.BINS_T FOR STRAP.BINS_T;
+
+-- Creation of DROP TABLE PALLETS_T 
+  CREATE TABLE STRAP.PALLETS_T 
+   (PALLET_ID                 VARCHAR2(16) NOT NULL, 
+	  STATUS                    VARCHAR2(20), 
+	  STATUS_DT                 DATE, 
+	  FROM_LOC                  VARCHAR2(5), 
+	  LABEL                     VARCHAR2(20), 
+	  INVOICE_NUM               VARCHAR2(20), 
+	  STATE                     VARCHAR2(20), 
+	  PART_NO                   VARCHAR2(20), 
+	  QTY                       NUMBER, 
+	  OWNER                     VARCHAR2(20), 
+	  SEQ                       NUMBER, 
+    PART_GRP                  VARCHAR2(4),
+	  CONSTRAINT PK_PALLETS PRIMARY KEY (PALLET_ID)
+   );
+   
+--CREATE OR REPLACE SYNONYM APPS.PALLETS_T FOR STRAP.PALLETS_T;   
+
+
+   -- Creation of table USERS_T
+   
+   CREATE TABLE STRAP.USERS_T
+   (  USER_ID                   VARCHAR2(8) NOT NULL, 
+	  PASSWORD                  VARCHAR2(8), 
+	  NAME                      VARCHAR2(70), 
+	  EMAIL                     VARCHAR2(70), 
+	  PHONE                     NUMBER(10,0), 
+	  ROLE                      VARCHAR2(20), 
+	  LOC_ID                    VARCHAR2(5), 
+	  PART_GRP                  VARCHAR2(4), 
+	  CONSTRAINT PK_USERS PRIMARY KEY (USER_ID)
+   ) ;  
+ 
+-- CREATE OR REPLACE SYNONYM APPS.USERS_T FOR STRAP.USERS_T;     
+ 
+ 
+ -- Creation of PARTS_T
+ 
+   CREATE TABLE STRAP.PARTS_T
+   (  PART_GRP                  VARCHAR2(4) NOT NULL, 
+      PART_NO                   VARCHAR2(20) NOT NULL, 
+	  CUST_PART_NO              VARCHAR2(20), 
+	  VARIANT                   VARCHAR2(20), 
+	  PARTS_TYPE                VARCHAR2(20), 
+	  SERIALIZED	            VARCHAR2(1) DEFAULT 'N',
+	 CONSTRAINT PK_MATERIAL PRIMARY KEY (PART_GRP, PART_NO)
+   );
+   
+  -- CREATE OR REPLACE SYNONYM APPS.PARTS_T FOR STRAP.PARTS_T;   
+   
+   -- Creation of ASN_T
+  CREATE TABLE STRAP.ASN_T 
+   (  ASN_ID                    VARCHAR2(20) NOT NULL , 
+	  ASN_LINE                  NUMBER NOT NULL, 
+	  ASN_DATE                  DATE NOT NULL , 
+      INVOICE_NUM               VARCHAR2(20), 
+	  CUST_PART_NO              VARCHAR2(20), 
+ 	  PART_NO                   VARCHAR2(20), 
+	  QTY                       NUMBER, 
+	  PART_GRP                  VARCHAR2(4), 
+	  USER_ID                   VARCHAR2(20),
+	 CONSTRAINT PK_ASN_TBL PRIMARY KEY (ASN_ID,ASN_LINE,ASN_DATE)
+  );
+  
+   --CREATE OR REPLACE SYNONYM APPS.ASN_T FOR STRAP.ASN_T; 
+   
+  CREATE TABLE STRAP.LOCATIONS_T 
+   (  LOC_ID                     VARCHAR2(5) NOT NULL, 
+	  DESCRIPTION                VARCHAR2(20), 
+	  TYPE                       VARCHAR2(20), 
+      CLOSE_STATUS               VARCHAR2(20),
+      LAT                        NUMBER,
+      LON                        NUMBER,
+    CONSTRAINT PK_LOCATIONS PRIMARY KEY (LOC_ID)
+   );
+     --CREATE OR REPLACE SYNONYM APPS.LOCATIONS_T FOR STRAP.LOCATIONS_T; 
+     
+     
+   CREATE TABLE STRAP.SCHED_T
+   (  SCHED_DT                  DATE NOT NULL, 
+	  SCHED_HR                  NUMBER DEFAULT 0 NOT NULL, 
+	  CUST_PART_NO              VARCHAR2(20) NOT NULL, 
+	  PART_NO                   VARCHAR2(20), 
+	  WIP_QTY                   NUMBER, 
+	  QTY                       NUMBER, 
+	  PART_GRP                  VARCHAR2(4), 
+	  USER_ID                   VARCHAR2(20), 
+	 CONSTRAINT PK_SCHED_TBL PRIMARY KEY (SCHED_DT,CUST_PART_NO, SCHED_HR)
+   );
+     --CREATE OR REPLACE SYNONYM APPS.SCHED_T FOR STRAP.SCHED_T; 
+     
+  /*   
+     CREATE TABLE STRAP.INV_PALLET_T
+   (  INVOICE_NUM             VARCHAR2(20) NOT NULL , 
+	  INV_DT                    VARCHAR2(20) NOT NULL , 
+	  LABEL                     VARCHAR2(20) NOT NULL , 
+	  OBJ_ID                    VARCHAR2(20), 
+	  PART_NO                   VARCHAR2(20), 
+	  QTY                       VARCHAR2(20), 
+	  LOC_ID                    VARCHAR2(5), 
+	  USER_ID                   VARCHAR2(20), 
+	  TYPE                      VARCHAR2(20), 
+	 CONSTRAINT PK_INV_PALLET PRIMARY KEY (INVOICE_NUM,INV_DT,LABEL)
+  );
+    --CREATE OR REPLACE SYNONYM APPS.INV_PALLET_T FOR STRAP.INV_PALLET_T;
+   --
+   */
+   -- DROP TABLE STRAP.UPLOAD_LOG_T 
+    CREATE TABLE STRAP.UPLOAD_LOG_T 
+   (  SEQ                       NUMBER NOT NULL, 
+	  TYPE                      VARCHAR2(20), 
+	  USER_ID                   VARCHAR2(20), 
+	  TOTAL                     NUMBER, 
+	  SUCCESS                   NUMBER, 
+	  ERROR                     NUMBER, 
+	  SYSFILENAME               VARCHAR2(50), 
+	  ORIGFILENAME              VARCHAR2(50), 
+	  LOGFILENAME               VARCHAR2(50), 
+	  DESCRIPTION               VARCHAR2(50), 
+	  PART_GRP                  VARCHAR2(4) NOT NULL,
+	  CONSTRAINT PK_UPLOAD_LOG PRIMARY KEY (SEQ)
+   );
+     --CREATE OR REPLACE SYNONYM APPS.UPLOAD_LOG_T FOR STRAP.UPLOAD_LOG_T; 
+    -- DROP TABLE STRAP.USER_PARTS_T
+   CREATE TABLE STRAP.USER_PARTS_T
+   ( USER_ID                   VARCHAR2(20) NOT NULL, 
+	 PART_GRP                  VARCHAR2(4) NOT NULL,
+	 CONSTRAINT PK_USER_PARTS PRIMARY KEY (USER_ID,PART_GRP)
+   );
+    --CREATE OR REPLACE SYNONYM APPS.USER_PARTS_T FOR STRAP.USER_PARTS_T; 
+    -- DROP TABLE STRAP.PARTS_GRP_T
+  CREATE TABLE STRAP.PARTS_GRP_T
+   (PART_GRP                   VARCHAR2(4) NOT NULL, 
+	DESCRIPTION                VARCHAR2(50), 
+    OWNER                      VARCHAR2(20) NOT NULL,
+	  CONSTRAINT PK_PART_GRP PRIMARY KEY (PART_GRP)
+   );
+    --CREATE OR REPLACE SYNONYM APPS.PARTS_GRP_T FOR STRAP.PARTS_GRP_T; 
+   -- Creation of XX_STRAP_INV_HDR_T
+
+  CREATE TABLE STRAP.INV_HDR_T 
+   (  INVOICE_NUM               VARCHAR2(20) NOT NULL, 
+	  INV_DT                    DATE NOT NULL , 
+	  FROM_LOC                  VARCHAR2(5), 
+	  TO_LOC                    VARCHAR2(5), 
+	  LR_NO                     VARCHAR2(20), 
+	  DEVICE_ID                 VARCHAR2(20), 
+	  STATUS                    VARCHAR2(20), 
+      PART_GRP                  VARCHAR2(4),
+      USER_ID                   VARCHAR2(20) NOT NULL, 
+	 CONSTRAINT PK_INV_HDR PRIMARY KEY (INVOICE_NUM, INV_DT)
+   );
+    
+--CREATE OR REPLACE SYNONYM APPS.INV_HDR_T FOR STRAP.INV_HDR_T;   
+
+  CREATE TABLE STRAP.INV_LINE_T 
+   (  INVOICE_NUM               VARCHAR2(20) NOT NULL, 
+	  INV_DT                    DATE NOT NULL, 
+	  LINE                      NUMBER NOT NULL, 
+	  PART_NO                   VARCHAR2(20), 
+	  QTY                       NUMBER,
+      PART_GRP                  VARCHAR2(4),
+	 CONSTRAINT PK_INV_LINE PRIMARY KEY (INVOICE_NUM,INV_DT,LINE)
+   );
+  
+--CREATE OR REPLACE SYNONYM APPS.INV_LINE_T FOR STRAP.INV_LINE_T;    
+
+-- 
+  CREATE TABLE STRAP.SERIAL_T 
+   ( SERIAL_DT	                DATE
+     ,SERIAL_NUM	            NUMBER
+     ,BIN_LABEL	                VARCHAR2(20)
+     ,BIN_ID	                VARCHAR2(16)
+     ,PART_NO	                VARCHAR2(20)	 
+	 ,PART_GRP                  VARCHAR2(4) 
+	 ,USER_ID                   VARCHAR2(20)
+	 ,CONSTRAINT PK_SER_N PRIMARY KEY (SERIAL_NUM,BIN_LABEL)
+   );
+  
+--CREATE OR REPLACE SYNONYM APPS.SERIAL_T FOR STRAP.SERIAL_T;  
---
+++ STRAP_TRIGGER_CREATION_SCRIPT.sql
@@ -0,0 +1,494 @@
+ /****************************************************************************************************
+  **  Name       : STRAP_TRIGGER_CREATION_SCRIPT.sql
+  **  DESCRIPTION: This script is used to create the Triggers in STRAP Project
+  **  MODIFICATION  HISTORY
+  **  Change Number   Version  Date           Modified By      Remarks
+  **  -               1.0      21-Aug-2017   Saif           Initial draft version
+  ****************************************************************************************************/
+
+CREATE OR REPLACE TRIGGER STRAP.AFTER_EVENTS_TRIG
+AFTER INSERT ON STRAP.EVENTS_T
+FOR EACH ROW
+DECLARE
+  lv_error_message VARCHAR2(1000);
+  PRAGMA AUTONOMOUS_TRANSACTION;
+BEGIN
+   
+	IF :NEW.EVENT_TYPE = 'Pallet' AND :NEW.EVENT_NAME='Palletised'
+	THEN 
+	    UPDATE PALLETS_T
+           SET STATUS='Palletised',
+               STATUS_DT=SYSDATE,
+               FROM_LOC=:NEW.FROM_LOC,
+               LABEL=:NEW.LABEL ,
+               PART_NO=:NEW.PART_NO ,
+               QTY=:NEW.QTY,
+               PART_GRP=:NEW.PART_GRP
+         WHERE PALLET_ID=:NEW.EVENT_ID
+           AND STATUS='Ready';
+    ELSIF :NEW.EVENT_TYPE = 'Bin' AND :NEW.EVENT_NAME='Palletised'
+    THEN 
+	    UPDATE BINS_T
+           SET STATUS='Palletised',
+               STATUS_DT=SYSDATE,
+               PALLET_ID =:NEW.REF_ID,
+               FROM_LOC=:NEW.FROM_LOC,
+               LABEL=:NEW.LABEL ,
+               PART_NO=:NEW.PART_NO ,
+               QTY=:NEW.QTY,
+               PART_GRP=:NEW.PART_GRP
+         WHERE BIN_ID=:NEW.EVENT_ID
+           AND STATUS='Ready';
+    ELSIF :NEW.EVENT_TYPE='Invoice' AND :NEW.event_name='Add'
+    THEN
+        INSERT 
+		  INTO INV_HDR_T
+                (
+                  INVOICE_NUM
+                 ,INV_DT
+                 ,FROM_LOC
+                 ,TO_LOC
+                 ,LR_NO
+                 ,DEVICE_ID
+                 ,STATUS
+                 ,PART_GRP
+                 ,USER_ID
+                )
+                VALUES
+                ( :NEW.EVENT_ID--INV_NUM
+                ,SYSDATE--INV_DT
+                ,:NEW.FROM_LOC
+                ,:NEW.TO_LOC
+                ,NULL--LR_NO
+                ,NULL--DEVICE_ID
+                ,'New'--STATUS
+                ,:NEW.PART_GRP--PART_GRP
+                ,:NEW.USER_ID--USER_ID
+                );
+    
+        INSERT 
+		  INTO INV_LINE_T
+               (
+                INVOICE_NUM
+               ,INV_DT
+               ,LINE
+               ,PART_NO
+               ,QTY
+               ,PART_GRP
+               )
+               VALUES
+              (:NEW.EVENT_ID--INV_NUM
+               ,SYSDATE
+              ,:NEW.COMMENTS
+              ,:NEW.PART_NO
+              ,:NEW.QTY
+              ,:NEW.PART_GRP
+              );
+    ELSIF :NEW.EVENT_TYPE='Invoice' AND :NEW.event_name='Delete'
+    THEN
+		DELETE 
+		  FROM INV_HDR_T
+		 WHERE INVOICE_NUM=:NEW.EVENT_ID;
+    
+        DELETE 
+		  FROM INV_LINE_T
+         WHERE INVOICE_NUM=:NEW.EVENT_ID;
+    ELSIF :NEW.EVENT_TYPE = 'Pallet' AND :NEW.EVENT_NAME='Release'
+    THEN 
+	    UPDATE PALLETS_T
+           SET STATUS='Ready',
+               STATUS_DT=SYSDATE,
+               FROM_LOC=:NEW.TO_LOC,
+               LABEL=NULL ,
+               PART_NO=NULL ,
+               QTY=0,
+               PART_GRP=NULL
+         WHERE PALLET_ID=:NEW.EVENT_ID;  
+    ELSIF :NEW.EVENT_TYPE = 'Bin' AND :NEW.EVENT_NAME='Release'
+    THEN 
+	    UPDATE BINS_T
+           SET STATUS='Ready',
+               STATUS_DT=SYSDATE,
+               PALLET_ID =NULL,
+               FROM_LOC=:NEW.TO_LOC,
+               LABEL=NULL ,
+               PART_NO=NULL ,
+               QTY=0,
+               PART_GRP=NULL
+         WHERE BIN_ID=:NEW.EVENT_ID;
+    ELSIF :NEW.EVENT_TYPE = 'Pallet' AND :NEW.EVENT_NAME='Invoice'
+    THEN
+        UPDATE INV_HDR_T
+           SET STATUS='Parts Assigned'
+         WHERE INVOICE_NUM=:NEW.INVOICE_NUM;
+    
+        UPDATE PALLETS_T
+           SET STATUS='Invoiced',  
+               STATUS_DT=SYSDATE,
+               INVOICE_NUM=:NEW.INVOICE_NUM,
+               PART_GRP=:NEW.PART_GRP
+         WHERE PALLET_ID=:NEW.EVENT_ID
+           AND STATUS='Palletised';  
+       
+        UPDATE BINS_T
+           SET STATUS='Invoiced',
+               STATUS_DT=SYSDATE,
+               INVOICE_NUM=:NEW.INVOICE_NUM,
+               PART_GRP=:NEW.PART_GRP
+         WHERE PALLET_ID=:NEW.EVENT_ID
+           AND STATUS='Palletised';
+       
+    ELSIF :NEW.EVENT_TYPE = 'Pallet' AND :NEW.EVENT_NAME='Invoice Rev'
+    THEN
+        UPDATE PALLETS_T
+           SET STATUS='Palletised',  
+               STATUS_DT=SYSDATE,
+               INVOICE_NUM=NULL,
+               PART_GRP=:NEW.PART_GRP
+         WHERE PALLET_ID=:NEW.EVENT_ID
+           AND STATUS='Invoiced';  
+       
+         UPDATE BINS_T
+            SET STATUS='Palletised',
+                STATUS_DT=SYSDATE,
+                INVOICE_NUM=NULL,
+                PART_GRP=:NEW.PART_GRP
+          WHERE PALLET_ID=:NEW.EVENT_ID
+            AND STATUS='Invoiced';     
+    ELSIF :NEW.EVENT_TYPE = 'Invoice' AND :NEW.EVENT_NAME='Invoice Rev'
+    THEN
+        UPDATE INV_HDR_T
+           SET STATUS='New'
+         WHERE INVOICE_NUM=:NEW.EVENT_ID
+           AND STATUS='Parts Assigned';
+    ELSIF :NEW.EVENT_TYPE = 'Bin' AND :NEW.EVENT_NAME='Invoice'
+    THEN
+        UPDATE INV_HDR_T
+           SET STATUS='Parts Assigned'
+         WHERE INVOICE_NUM=:NEW.INVOICE_NUM;
+       
+        UPDATE BINS_T
+           SET STATUS='Invoiced',
+               STATUS_DT=SYSDATE,
+               LABEL=NVL(:NEW.LABEL,LABEL),
+               INVOICE_NUM=:NEW.INVOICE_NUM,
+               PART_GRP=:NEW.PART_GRP
+         WHERE BIN_ID=:NEW.EVENT_ID;
+       
+    ELSIF :NEW.EVENT_TYPE = 'Bin' AND :NEW.EVENT_NAME='Invoice Rev'
+    THEN
+     UPDATE BINS_T
+      SET STATUS='Palletised',
+          STATUS_DT=SYSDATE,
+          INVOICE_NUM=NULL,
+          PART_GRP=:NEW.PART_GRP
+      WHERE PALLET_ID=:NEW.EVENT_ID
+       AND STATUS='Invoiced'; 
+       
+  ELSIF :NEW.EVENT_TYPE = 'Invoice' AND :NEW.EVENT_NAME='LR Assigned'
+  THEN
+   UPDATE INV_HDR_T
+      SET STATUS='LR Assigned',
+          LR_NO =:NEW.LR_NO,
+          DEVICE_ID =:NEW.DEVICE_NO
+    WHERE INVOICE_NUM=:NEW.EVENT_ID; 
+  ELSIF :NEW.EVENT_TYPE = 'Invoice' AND :NEW.EVENT_NAME='LR Assigned Rev'
+  THEN
+   UPDATE INV_HDR_T
+      SET STATUS='Parts Assigned',
+          LR_NO =NULL,
+          DEVICE_ID =NULL
+    WHERE INVOICE_NUM=:NEW.EVENT_ID
+      AND STATUS='LR Assigned'; 
+  ELSIF :NEW.EVENT_TYPE = 'Pallet' AND :NEW.EVENT_NAME='Dispatched'
+  THEN 
+     UPDATE PALLETS_T
+      SET STATUS='Dispatched',  
+          STATUS_DT=SYSDATE,
+          INVOICE_NUM=:NEW.INVOICE_NUM,
+          PART_GRP=:NEW.PART_GRP
+      WHERE PALLET_ID=:NEW.EVENT_ID;  
+       
+     UPDATE BINS_T
+      SET STATUS='Dispatched',
+          STATUS_DT=SYSDATE,
+          INVOICE_NUM=:NEW.INVOICE_NUM,
+          PART_GRP=:NEW.PART_GRP
+      WHERE PALLET_ID=:NEW.EVENT_ID; 
+  ELSIF :NEW.EVENT_TYPE = 'Bin' AND :NEW.EVENT_NAME='Dispatched'
+  THEN        
+     UPDATE BINS_T
+      SET STATUS='Dispatched',
+          STATUS_DT=SYSDATE,
+          INVOICE_NUM=:NEW.INVOICE_NUM,
+          PART_GRP=:NEW.PART_GRP
+      WHERE BIN_ID=:NEW.EVENT_ID; 
+  ELSIF :NEW.EVENT_TYPE = 'Invoice' AND :NEW.EVENT_NAME='Dispatched'
+  THEN
+   UPDATE INV_HDR_T
+      SET STATUS='Dispatched'
+    WHERE INVOICE_NUM=:NEW.EVENT_ID; 
+	/* For Manual Reached without geoFence*/
+  ELSIF :NEW.EVENT_TYPE = 'Invoice' AND :NEW.EVENT_NAME='Reached'
+  THEN 
+	 UPDATE INV_HDR_T
+         SET STATUS='Reached'
+             --LR_NO =NULL,
+             --DEVICE_ID =NULL
+       WHERE INVOICE_NUM=:NEW.EVENT_ID; 
+    
+      UPDATE PALLETS_T
+         SET STATUS='Reached',  
+             STATUS_DT=SYSDATE,
+             FROM_LOC=:NEW.FROM_LOC,
+             INVOICE_NUM=:NEW.INVOICE_NUM,
+             PART_GRP=:NEW.PART_GRP
+       WHERE INVOICE_NUM=:NEW.EVENT_ID;  
+       
+      UPDATE BINS_T
+         SET STATUS='Reached',
+             STATUS_DT=SYSDATE,
+             FROM_LOC=:NEW.FROM_LOC,
+             INVOICE_NUM=:NEW.INVOICE_NUM,
+             PART_GRP=:NEW.PART_GRP
+       WHERE INVOICE_NUM=:NEW.EVENT_ID;
+  ELSIF :NEW.EVENT_TYPE = 'Invoice' AND :NEW.EVENT_NAME='Geofence'
+  THEN 
+    IF :NEW.FROM_LOC <>:NEW.TO_LOC
+      THEN
+        NULL;
+    ELSE
+      UPDATE INV_HDR_T
+         SET STATUS='Reached'
+            -- LR_NO =NULL,
+            -- DEVICE_ID =NULL
+       WHERE INVOICE_NUM=:NEW.EVENT_ID; 
+    
+      UPDATE PALLETS_T
+         SET STATUS='Reached',  
+             STATUS_DT=SYSDATE,
+             FROM_LOC=:NEW.FROM_LOC,
+             INVOICE_NUM=:NEW.INVOICE_NUM,
+             PART_GRP=:NEW.PART_GRP
+       WHERE INVOICE_NUM=:NEW.EVENT_ID;  
+       
+      UPDATE BINS_T
+         SET STATUS='Reached',
+             STATUS_DT=SYSDATE,
+             FROM_LOC=:NEW.FROM_LOC,
+             INVOICE_NUM=:NEW.INVOICE_NUM,
+             PART_GRP=:NEW.PART_GRP
+       WHERE INVOICE_NUM=:NEW.EVENT_ID;
+      END IF;
+  ELSIF :NEW.EVENT_TYPE IN ('Pallet','Bin','Invoice') AND :NEW.EVENT_NAME='Received'
+  THEN
+    IF :NEW.EVENT_TYPE='Pallet'
+    THEN
+     UPDATE PALLETS_T
+         SET STATUS='In Stock',  
+             STATUS_DT=SYSDATE,
+             INVOICE_NUM=:NEW.INVOICE_NUM,
+             FROM_LOC=:NEW.FROM_LOC,
+             PART_GRP=:NEW.PART_GRP
+       WHERE PALLET_ID=:NEW.EVENT_ID;  
+       
+      UPDATE BINS_T
+         SET STATUS='In Stock',
+             STATUS_DT=SYSDATE,
+             INVOICE_NUM=:NEW.INVOICE_NUM,
+             FROM_LOC=:NEW.FROM_LOC,
+             PART_GRP=:NEW.PART_GRP
+       WHERE PALLET_ID=:NEW.EVENT_ID;
+     ELSIF :NEW.EVENT_TYPE='Bin'
+     THEN
+      UPDATE BINS_T
+         SET STATUS='In Stock',
+             STATUS_DT=SYSDATE,
+             INVOICE_NUM=:NEW.INVOICE_NUM,
+             FROM_LOC=:NEW.FROM_LOC,
+             PART_GRP=:NEW.PART_GRP
+       WHERE BIN_ID=:NEW.EVENT_ID;
+      ELSIF :NEW.EVENT_TYPE='Invoice'
+      THEN
+       UPDATE INV_HDR_T
+         SET STATUS='Received',
+             LR_NO =NULL,
+             DEVICE_ID =NULL
+       WHERE INVOICE_NUM=:NEW.EVENT_ID; 
+      ELSE
+        NULL;
+      END IF;
+  ELSIF :NEW.EVENT_TYPE IN ('Pallet','Bin') AND :NEW.EVENT_NAME='Review'
+  THEN
+    IF :NEW.EVENT_TYPE='Pallet'
+    THEN
+     UPDATE PALLETS_T
+         SET STATUS='Review',  
+             STATUS_DT=SYSDATE,
+             INVOICE_NUM=:NEW.INVOICE_NUM,
+             FROM_LOC=:NEW.FROM_LOC,
+             PART_GRP=:NEW.PART_GRP
+       WHERE PALLET_ID=:NEW.EVENT_ID;  
+       
+      UPDATE BINS_T
+         SET STATUS='Review',
+             STATUS_DT=SYSDATE,
+             INVOICE_NUM=:NEW.INVOICE_NUM,
+             FROM_LOC=:NEW.FROM_LOC,
+             PART_GRP=:NEW.PART_GRP
+       WHERE PALLET_ID=:NEW.EVENT_ID;
+     ELSIF :NEW.EVENT_TYPE='Bin'
+     THEN
+      UPDATE BINS_T
+         SET STATUS='Review',
+             STATUS_DT=SYSDATE,
+             INVOICE_NUM=:NEW.INVOICE_NUM,
+             FROM_LOC=:NEW.FROM_LOC,
+             PART_GRP=:NEW.PART_GRP
+       WHERE BIN_ID=:NEW.EVENT_ID;
+      ELSE
+        NULL;
+      END IF;
+  ELSIF :NEW.EVENT_TYPE ='Bin' AND :NEW.EVENT_NAME='Picked' AND :NEW.EVENT_ID <> :NEW.REF_ID
+  THEN
+      UPDATE BINS_T
+         SET STATUS='Picked',
+             STATUS_DT=SYSDATE,
+             INVOICE_NUM=NULL,
+             FROM_LOC=:NEW.FROM_LOC,
+             PART_GRP=:NEW.PART_GRP,
+             QTY=QTY+:NEW.QTY
+       WHERE BIN_ID=:NEW.EVENT_ID;
+  ELSIF :NEW.EVENT_TYPE ='Bin' AND :NEW.EVENT_NAME='Transferred' AND :NEW.EVENT_ID <> :NEW.REF_ID
+  THEN    
+        UPDATE BINS_T
+         SET QTY=QTY-:NEW.QTY
+       WHERE BIN_ID=:NEW.EVENT_ID;
+  ELSIF :NEW.EVENT_TYPE ='Bin' AND :NEW.EVENT_NAME='Picked'
+  THEN
+      UPDATE BINS_T
+         SET STATUS='Picked',
+             STATUS_DT=SYSDATE,
+             INVOICE_NUM=NULL,
+             FROM_LOC=:NEW.FROM_LOC,
+             PART_GRP=:NEW.PART_GRP,
+             QTY=:NEW.QTY
+       WHERE BIN_ID=:NEW.EVENT_ID;
+  ELSIF :NEW.EVENT_TYPE IN ('Invoice') AND :NEW.EVENT_NAME='ASN Assigned'
+  THEN
+  INSERT INTO ASN_T
+  (
+    ASN_ID
+   ,ASN_LINE	
+   ,ASN_DATE	
+   ,INVOICE_NUM	
+   ,CUST_PART_NO	
+   ,PART_NO	
+   ,QTY	
+   ,PART_GRP	
+   ,USER_ID	
+   )
+   VALUES
+   ( 	
+    :NEW.ref_label--ASN_ID
+   ,:NEW.comments
+   , SYSDATE	
+   ,:NEW.EVENT_ID	
+   ,:NEW.PART_NO	
+   ,:NEW.PART_NO	
+   ,:NEW.QTY	
+   ,:NEW.PART_GRP	
+   ,:NEW.USER_ID	
+   );
+   
+   UPDATE INV_HDR_T
+      SET STATUS='ASN Assigned',
+          LR_NO =NULL,
+          DEVICE_ID =NULL
+    WHERE INVOICE_NUM=:NEW.EVENT_ID;
+  ELSIF :NEW.EVENT_TYPE IN ('Invoice') AND :NEW.EVENT_NAME='ASN Delete'
+  THEN
+     DELETE FROM ASN_T
+      WHERE INVOICE_NUM =:NEW.EVENT_ID;
+   
+   UPDATE INV_HDR_T
+      SET STATUS='New'
+    WHERE INVOICE_NUM=:NEW.EVENT_ID; 
+	END IF;
+  COMMIT;
+EXCEPTION
+WHEN OTHERS THEN
+raise_application_error (-20001,'Error ! When Updating :'||SQLERRM);   
+END;
+/
+Show Err;
+/
+
+
+ -- Create Before Schedule Trigger
+CREATE OR REPLACE TRIGGER STRAP.BEFORE_SCHED_TRIG
+BEFORE INSERT
+ON SCHED_T
+FOR EACH ROW
+DECLARE v_partNo VARCHAR2(20);
+
+BEGIN
+   -- Get the Part Number
+   BEGIN
+    SELECT PART_NO 
+      INTO v_partNo 
+	 FROM PARTS_T 
+	WHERE CUST_PART_NO = :new.CUST_PART_NO;
+   EXCEPTION
+   WHEN OTHERS THEN
+     v_partNo :=NULL;
+   END;
+   
+   :new.PART_NO := v_partNo;
+   
+EXCEPTION
+WHEN NO_DATA_FOUND THEN
+raise_application_error (-20001,'No data available for this customer part number');   
+END;
+
+-- Create Before Serial Trigger
+CREATE OR REPLACE TRIGGER STRAP.BEFORE_SERIAL_TRIG
+BEFORE INSERT ON SERIAL_T
+FOR EACH ROW
+DECLARE 
+v_partNo VARCHAR2(20);
+v_binid  VARCHAR2(16);
+BEGIN
+   -- Get the Part Number
+   BEGIN
+    SELECT PART_NO 
+      INTO v_partNo 
+	  FROM PARTS_T 
+	 WHERE PART_NO = SUBSTR(:new.PART_NO,1,4)||'.'||SUBSTR(:new.PART_NO,5,3)||'.'||SUBSTR(:new.PART_NO,8,3)||'-'||SUBSTR(:new.PART_NO,11,3);
+	-- WHERE REGEXP_REPLACE(PART_NO ,'[^[:alnum:]'' '']', NULL) = :new.PART_NO;
+   EXCEPTION 
+   WHEN OTHERS THEN
+     v_partNo :=NULL;
+   END;
+   
+   :new.PART_NO := v_partNo;
+   
+   BEGIN
+    SELECT BIN_ID 
+      INTO v_binid 
+	  FROM BINS_T 
+	 WHERE LABEL = :new.BIN_LABEL;
+   EXCEPTION
+   WHEN OTHERS THEN
+     v_binid :=NULL;
+   END;
+   
+   :new.BIN_ID := v_binid;
+   
+EXCEPTION
+WHEN NO_DATA_FOUND THEN
+raise_application_error (-20001,'No data available for this Serial Number :');   
+END;
+/
+Show Err;
+/
